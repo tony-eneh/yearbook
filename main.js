bfdca9a2f1ad3b90225c6b2370693849012e0881
Tony Eneh <aeneh90@gmail.com>
2020-07-16 21:40:32 +0100

    feat: brighten image on expansion

--- main.js
+++ main.js
@@ -10,6 +10,23 @@ const showDetail = (e) => {
 
   //   display close button
   closeButton.classList.remove("hide");
+
+  //   undarken the image
+  e.currentTarget.querySelector("figcaption").classList.add("undarken");
+};
+
+const closeDetail = (e) => {
+  // hide close button
+  closeButton.classList.add("hide");
+
+  // display grid items
+  //   also remove the expanded class if it exists. (it will exist for the currently expande element)
+  gridItems.forEach((item) => {
+    item.classList.remove("hide", "expanded-item");
+    //   redarken the image
+    item.querySelector("figcaption").classList.remove("undarken");
+  });
 };
 
 gridItems.forEach((item) => item.addEventListener("click", showDetail));
+closeButton.addEventListener("click", closeDetail);
